f189fe936caa326ad1da6aa8bba1547aaf56bb0e
Salai Rares <salaicostel@yahoo.com>
2020-06-20 00:07:07 +0300

    created Controllers Package + created Produs Controller and executable scene for creating produs

--- src/main/java/Controllers/ProdMain.java
+++ src/main/java/Controllers/ProdMain.java
@@ -1,16 +1,27 @@
 package Controllers;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ProdMain extends Application {
-
+private Stage primaryStage;
+private AnchorPane anchorPane;
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws Exception {
+       FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/photochooser.fxml"));
+        anchorPane=loader.load();
+        Scene scene =new Scene(anchorPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
