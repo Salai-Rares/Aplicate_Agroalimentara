1572fb150213fcbe08825f656fb428ec652e5c57
Salai Rares <salaicostel@yahoo.com>
2020-06-20 00:05:19 +0300

    created Categorie model

--- src/main/java/Models/Categorie.java
+++ src/main/java/Models/Categorie.java
@@ -1,4 +1,30 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categorie {
+    private String pic_path;
+    protected List<Produs> produse=new ArrayList<>();
+
+    public Categorie(String pic_path, List<Produs> produse) {
+        this.pic_path = pic_path;
+        this.produse = produse;
+    }
+
+    public String getPic_path() {
+        return pic_path;
+    }
+
+    public void setPic_path(String pic_path) {
+        this.pic_path = pic_path;
+    }
+
+    public List<Produs> getProduse() {
+        return produse;
+    }
+
+    public void setProduse(List<Produs> produse) {
+        this.produse = produse;
+    }
 }
