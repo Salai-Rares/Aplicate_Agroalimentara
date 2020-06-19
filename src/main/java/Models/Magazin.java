78dc9f57e3f089cce87ac21ab7fd38b17b3a470c
Salai Rares <salaicostel@yahoo.com>
2020-06-20 00:04:40 +0300

    created magazin model

--- src/main/java/Models/Magazin.java
+++ src/main/java/Models/Magazin.java
@@ -1,4 +1,30 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Magazin {
+    private String mag_pic_path;
+    protected List<Categorie> categori=new ArrayList<>();
+
+    public Magazin(String mag_pic_path, List<Categorie> categori) {
+        this.mag_pic_path = mag_pic_path;
+        this.categori = categori;
+    }
+
+    public String getMag_pic_path() {
+        return mag_pic_path;
+    }
+
+    public void setMag_pic_path(String mag_pic_path) {
+        this.mag_pic_path = mag_pic_path;
+    }
+
+    public List<Categorie> getCategori() {
+        return categori;
+    }
+
+    public void setCategori(List<Categorie> categori) {
+        this.categori = categori;
+    }
 }
