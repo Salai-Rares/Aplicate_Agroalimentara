9c42880093ccf2a4c840054981a600cb89ef452f
Salai Rares <salaicostel@yahoo.com>
2020-06-20 18:39:53 +0300

    created controller for categories and created an comunication class for stages

---
+++ src/main/java/JSON/CommunicationClass.java
@@ -0,0 +1,13 @@
+package JSON;
+
+public class CommunicationClass {
+    private static String username;
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        CommunicationClass.username = username;
+    }
+}
