8a7131edad3c8a2a3735b6d058f0c1d0e57f983c
Salai Rares <salaicostel@yahoo.com>
2020-07-01 17:44:16 +0300

    Created Login and Register Controllers

--- src/main/java/Controllers/ControllerLogin.java
+++ src/main/java/Controllers/ControllerLogin.java
@@ -1,4 +1,82 @@
 package Controllers;
 
+import AbstractClasses.AlertBox;
+import Exceptions.UsernameOrPasswordWrongException;
+import JSON.CommunicationClass;
+import JSON.UserSer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+import static AbstractClasses.AlertBox.showAlert;
+
 public class ControllerLogin {
+    @FXML
+    private AnchorPane anchfx;
+    @FXML
+    private TextField userfx;
+    @FXML
+    private PasswordField passfx;
+    @FXML
+    private Button inchidefx;
+    @FXML
+    private Hyperlink hyperfx;
+    @FXML
+    private Button salveazafx;
+    private UserSer userSer;
+    private HomePageMagazinMain homePageMagazinMain;
+    private HomePageCumparatorMain homePageCumparatorMain;
+   // private  Stage stage = (Stage) inchidefx.getScene().getWindow();
+    public void inchide_fereastra(MouseEvent event){
+        Stage stage = (Stage) inchidefx.getScene().getWindow();
+        stage.close();
+    }
+    public void setHyperfx(MouseEvent event) throws InterruptedException {
+        hyperfx.setVisited(false);
+        RegisterMain registerMain=new RegisterMain();
+        Stage stage = (Stage) inchidefx.getScene().getWindow();
+        try{
+
+        registerMain.start(stage);}catch (Exception exception){
+            exception.printStackTrace();
+        }
+
+    }
+    public void ButtonAction(ActionEvent event){
+        userSer=new UserSer();
+        if (userfx.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la autentificare!", "Introduceti un username!");
+            return;
+        }
+        if (passfx.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la autentificare!", "Introduceti o parola!");
+            return;
+        }
+        try {
+            String rol=userSer.checkCredentials(userfx.getText(),passfx.getText());
+            Stage stage=(Stage) salveazafx.getScene().getWindow();
+            stage.close();
+            homePageMagazinMain=new HomePageMagazinMain();
+            if(Objects.equals(rol,"Magazin")){
+                CommunicationClass.setUsername(userfx.getText());
+
+                homePageMagazinMain.start(stage);
+            }
+            else if(Objects.equals(rol,"Cumparator")){
+                homePageCumparatorMain=new HomePageCumparatorMain();
+                homePageCumparatorMain.start(stage);
+            }
+        }catch (UsernameOrPasswordWrongException ex){
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la autentificare!", "Date de autentificare gresite!");
+            return;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/Controllers/ControllerRegister.java
+++ src/main/java/Controllers/ControllerRegister.java
@@ -1,4 +1,114 @@
 package Controllers;
 
-public class ControllerRegister {
+import Exceptions.UsernameAlreadyExistsException;
+import JSON.CryptWithMD5;
+import JSON.UserSer;
+import Models.User;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static AbstractClasses.AlertBox.showAlert;
+
+public class ControllerRegister implements Initializable {
+    @FXML
+    private Button salveazafx;
+    @FXML
+    private Button inchidefx;
+    @FXML
+    private TextField userfx;
+    @FXML
+    private TextField emailfx;
+    @FXML
+    private PasswordField passfx;
+    @FXML
+    private PasswordField confirmfx;
+    @FXML
+    private TextField numefx;
+    @FXML
+    private TextField telefonfx;
+    @FXML
+    private ChoiceBox rolfx;
+    @FXML
+    private AnchorPane anchfx;
+    LoginMain loginMain=new LoginMain();
+    private UserSer userSer;
+    private User user;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        rolfx.setItems(FXCollections.observableArrayList("Cumparator", new Separator(), "Magazin"));
+    }
+    public void inchide_fereastra(MouseEvent event){
+        Stage stage = (Stage) inchidefx.getScene().getWindow();
+        stage.close();
+        try {
+            loginMain.start(stage);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void ButtonAction(ActionEvent event) throws InterruptedException {
+        if (emailfx.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la inregistrare!!", "Introduceti un email!");
+            return;
+        }
+        if (userfx.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la inregistrare!!", "Introduceti un username!");
+            return;
+        }
+        if (passfx.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la inregistrare!!", "Introduceti o parola!");
+            return;
+        }
+        if (confirmfx.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "EEroare la inregistrare!!", "Confirmati parola!");
+            return;
+        }
+        if (numefx.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la inregistrare!!", "Introduceti un nume!");
+            return;
+        }
+        if (telefonfx.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la inregistrare!", "Introduceti un telefon!");
+            return;
+        }
+        if (rolfx.getSelectionModel().getSelectedItem()==null) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la inregistrare!", "Introduceti un rol!");
+            return;
+        }
+        if(!passfx.getText().equals(confirmfx.getText())){
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la inregistrare!", "Parolele nu corespund!");
+            return;
+        }
+        user=new User(userfx.getText(), CryptWithMD5.cryptWithMD5(passfx.getText()),numefx.getText(),emailfx.getText(),telefonfx.getText(),rolfx.getSelectionModel().getSelectedItem().toString());
+        userSer=new UserSer();
+        try {
+            userSer.addUsers(user);
+        } catch (UsernameAlreadyExistsException e) {
+            showAlert(Alert.AlertType.ERROR, anchfx.getScene().getWindow(), "Eroare la inregistrare!", "Username-ul exista deja!");
+            return;
+        }
+        showAlert(Alert.AlertType.CONFIRMATION, anchfx.getScene().getWindow(), "Registration Successful!", "Welcome " + userfx.getText());
+        Thread.sleep(2000);
+        Stage stage=(Stage) salveazafx.getScene().getWindow();
+        stage.close();
+
+        try {
+            loginMain.start(stage);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
