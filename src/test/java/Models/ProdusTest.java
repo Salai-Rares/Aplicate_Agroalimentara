0be7b3b2e5d0144bb53cf9100ef69aa1c438079b
Salai Rares <salaicostel@yahoo.com>
2020-07-01 22:40:48 +0300

    created tests for Produs class

--- src/test/java/Models/ProdusTest.java
+++ src/test/java/Models/ProdusTest.java
@@ -1,5 +1,25 @@
+package Models;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ProdusTest {
+    private Produs produs;
+    @Before
+    public void setUp() throws Exception {
+       produs=new Produs("sa",3,"sad","as");
+    }
+    @Test
+    public void setPret() {
+        try {
+            produs.setPret(-2);
+            fail("nu merge");
+        }catch (IllegalArgumentException illegalArgumentException){
+            System.out.println("Passed!");
+        }
+    }
 
 }
